2efeae8652418544f54ec5574258822ef9f73466
Shashank <shashuhgowda444@gmail.com>
2026-01-13 12:54:05 +0530

    Update links to match the Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Home from './pages/HomePage'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route index element={<Home />}></Route>
+      <Route path='/checkout' element={<CheckoutPage />} />
+      <Route path='/orders' element={<OrdersPage />} />
+      <Route path='/tracking' element={<TrackingPage />} />
+    </Routes>
   )
 }
 
