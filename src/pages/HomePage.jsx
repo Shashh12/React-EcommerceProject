fc43e92cd417f77af4c4e24abb81e6f05aee12c3
Shashank <shashuhgowda444@gmail.com>
2026-01-14 14:44:15 +0530

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,16 @@ import products from "../../starting-code/data/products";
 
 
 function HomePage() {
+
+    fetch("http://localhost:3000/api/products")
+        .then((response) => {
+            response.json()
+        }).then((data) => {
+            console.log(data);
+
+        })
+
+
     return (
         <>
 
